f665d49a8d5b9560e67e1e4067dcd4b29f91122c
yejuan <nicegalhei222>
2019-04-26 14:31:48 +0800

    dev server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import _ from 'lodash';
 import './assets/style.css';
 import Icon from './assets/images/timg.jpg';
+import Data from './data.xml';
 
+import printMe from './js/print'
+printMe();
 function component(){
     var element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -10,6 +13,13 @@ function component(){
     var myicon = new Image();
     myicon.src=Icon;
     element.appendChild(myicon);
+
+    //console.log(Data);
+
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onClick = printMe;
+    element.appendChild(btn);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry:'./src/index.js',
+    entry:{
+        app:'./src/index.js',
+        print:'./src/js/print.js'
+    },
+    devtool: 'inline-source-map',//前台报错可以直接查看到哪个文件报错
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 8080
+    },
     output: {
-        filename: "main.js",
+        filename: '[name]-[hash:8].bundle.js',
         path:path.resolve(__dirname,'dist'),
-        publicPath: "http://localhost:63342/wbp/dist/"
+        publicPath: "/"
     },
     module:{
         rules:[
@@ -16,7 +27,38 @@ module.exports = {
             {
                 test:/\.(png|svg|jpg|gif)$/,
                 use:['file-loader']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use:['file-loader']
+            },
+            {
+                test:/\.(csv|tsv)$/,
+                use:['csv-loader']
+            },
+            {
+                test: /\.xml$/,
+                use:['xml-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title:'Output Management',
+            filename:'index.html',
+            template:'index.html',
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true
+                // more options:
+                // https://github.com/kangax/html-minifier#options-quick-reference
+            },
+            // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+            chunksSortMode: 'dependency',
+            chunks:['app'] // 入口模块
+        })
+    ]
 };
