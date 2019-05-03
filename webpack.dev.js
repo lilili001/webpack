4681046baeb8112cf12eb0edf0b15b92a9927e3c
lilili001 <2861166132@qq.com>
2019-05-03 16:00:24 +0800

    环境分离

---
+++ webpack.dev.js
@@ -0,0 +1,19 @@
+const {smart} = require('webpack-merge');
+const base = require('./webpack.config');
+const path = require('path');
+
+module.exports = smart(base,{
+    mode:'development',
+    devtool:'source-map',
+    devServer: {//开发环境下
+        contentBase: path.join(__dirname, "dist"),//静态服务文件夹
+        compress: true,
+        port: 8080,
+        proxy:{
+            /*'api':{
+             target:'http://localhost:3000',
+             pathRewrite:{'/api':''}//可以过滤api前缀
+             }*/
+        }
+    },
+})
---
+++ webpack.prod.js
@@ -0,0 +1,18 @@
+const {smart} = require('webpack-merge');
+const base = require('./webpack.config');
+
+module.exports = smart(base,{
+    mode:'production',
+    //用于生产的
+    optimization: {
+        //用了optimize-css-assets-webpack-plugin这个插件就必须用js压缩插件,否则js不会被压缩
+        minimizer: [
+            new TerserJSPlugin({
+                cache:true,
+                parallel:true,//是否是并发打包的
+                sourceMap:true,//源码映射
+                exclude:/\/node_modules/
+            }),
+            new OptimizeCSSAssetsPlugin({})],
+    },
+})
