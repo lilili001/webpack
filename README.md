02223132993aa5ab377f3c76f0ad305dd86befb6
lilili001 <2861166132@qq.com>
2019-05-03 11:58:04 +0800

    css,js优化, 压缩

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - 自动刷新
 - 自动发布
 
-1.初始化项目
+1. 初始化项目
 ```
 npm init -y
 npm i -D webpack webpack-cli
@@ -13,8 +13,71 @@ npm i -D webpack webpack-cli
 
 webpack 默认支持js模块化,```module.exports,require(xx)``` 
 
+2. 处理样式
+
+- css : style-loader,css-loader
+
 ```$xslt
-module.exports = {
-    
-}
+ {
+                test:/\.css$/,
+                use:[{
+                    loader:'style-loader',
+                    options:{
+                        insertAt:'top' //插入到header最顶部
+                    }
+                },'css-loader']
+            },
 ```
+
+- less: less less-loader
+```$xslt
+{
+                test:/\.less/,
+                use:[
+                        {
+                            loader:'style-loader',
+                            options:{
+                                insertAt:'top' //插入到header最顶部
+                            }
+                        },
+                        'css-loader',
+                        'less-loader'
+                    ]
+            },
+```
+
+- scss: node-sass sass-loader
+
+```$xslt
+{
+                test:/\.scss/,
+                use:[
+                    {
+                        loader:'style-loader',
+                        options:{
+                            insertAt:'top' //插入到header最顶部
+                        }
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+```
+>> style-loader 处理的样式默认以style的形式插入到了head中,如何使用link的形式呢
+```$xslt
+//引入mini-css-extract-plugin
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');//可以定义多个
+
+//plugins中：
+new MiniCssExtractPlugin({
+            filename: 'main.css'
+        }),
+        
+//修改style-loader为以下：less,sass同理
+{
+    test: /\.css$/,
+    use: [MiniCssExtractPlugin.loader,'post-css',  'css-loader']
+},
+```
+
+autoprefixer:postcss-loader
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+//压缩js
+const TerserJSPlugin = require('terser-webpack-plugin');
+// 抽离style为link的css
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// 压缩　css
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 module.exports = {
-    mode:"development",//默认两种模式 production,development
-    entry:{
-        app:'./src/index.js',
-        print:'./src/js/print.js'
+    mode: "production",//默认两种模式 production,development
+    entry: {
+        app: './src/index.js',
+        print: './src/js/print.js'
     },
     devtool: 'inline-source-map',//前台报错可以直接查看到哪个文件报错
     devServer: {//开发环境下
@@ -17,70 +24,60 @@ module.exports = {
     },
     output: {
         filename: '[name]-[hash:8].bundle.js',
-        path:path.resolve(__dirname,'dist'),//路径必须是一个绝对路径
+        path: path.resolve(__dirname, 'dist'),//路径必须是一个绝对路径
         publicPath: "/"
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.css$/,
-                use:[{
-                    loader:'style-loader',
-                    options:{
-                        insertAt:'top' //插入到header最顶部
-                    }
-                },'css-loader']
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
             },
             {
-                test:/\.less/,
-                use:[
-                        {
-                            loader:'style-loader',
-                            options:{
-                                insertAt:'top' //插入到header最顶部
-                            }
-                        },
-                        'css-loader',
-                        'less-loader'
-                    ]
-            },
-            {
-                test:/\.scss/,
-                use:[
-                    {
-                        loader:'style-loader',
-                        options:{
-                            insertAt:'top' //插入到header最顶部
-                        }
-                    },
+                test: /\.less/,
+                use: [
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
+                    'postcss-loader',//解析css之前就加上前缀
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.scss/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',//解析css之前就加上前缀
                     'sass-loader'
                 ]
             },
             {
-                test:/\.(png|svg|jpg|gif)$/,
-                use:['file-loader']
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use:['file-loader']
+                use: ['file-loader']
             },
             {
-                test:/\.(csv|tsv)$/,
-                use:['csv-loader']
+                test: /\.(csv|tsv)$/,
+                use: ['csv-loader']
             },
             {
                 test: /\.xml$/,
-                use:['xml-loader']
+                use: ['xml-loader']
             }
         ]
     },
     plugins: [
         new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name]-[hash:8].css'
+        }),
         new HtmlWebpackPlugin({
-            title:'Output Management',
-            filename:'index.html',
-            template:'index.html',
+            title: 'Output Management',
+            filename: 'index.html',
+            template: 'index.html',
             inject: true,
             minify: {
                 removeComments: true,
@@ -89,11 +86,22 @@ module.exports = {
                 // more options:
                 // https://github.com/kangax/html-minifier#options-quick-reference
             },
-            hash:true,
+            hash: true,
             // necessary to consistently work with multiple chunks via CommonsChunkPlugin
             chunksSortMode: 'dependency',
-            chunks:['app'] // 入口模块
+            chunks: ['app'] // 入口模块
         })
-    ]
+    ],
+    optimization: {
+        //用了optimize-css-assets-webpack-plugin这个插件就必须用js压缩插件,否则js不会被压缩
+        minimizer: [
+            new TerserJSPlugin({
+                cache:true,
+                parallel:true,//是否是并发打包的
+                sourceMap:true,//源码映射
+                exclude:/\/node_modules/
+            }),
+            new OptimizeCSSAssetsPlugin({})],
+    },
 };
 
